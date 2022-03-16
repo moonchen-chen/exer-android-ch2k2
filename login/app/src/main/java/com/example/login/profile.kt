ac50f8597fce1a2e22f72f6aebfcaf65a095fb67
Khang Tran <95342544+kiemtiendaugia@users.noreply.github.com>
2022-03-16 15:40:09 +0700

    Add files via upload

---
+++ login/app/src/main/java/com/example/login/profile.kt
@@ -0,0 +1,15 @@
+package com.example.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_profile.*
+
+class profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+        changeimage.setOnClickListener {
+
+        }
+    }
+}
